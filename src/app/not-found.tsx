092286a7d92017306e1f818e0a7cf9a16e81137b
kingsuk <kingsuk@github.com>
2024-03-09 07:59:51 +0530

    fix - link issue

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import Link from "next/link";
 
 const NotFound = () => {
   return (
@@ -9,18 +9,18 @@ const NotFound = () => {
             404
           </h1>
           <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl ">
-            Something's missing.
+            Something missing.
           </p>
           <p className="mb-4 text-lg font-light text-gray-500 ">
-            Sorry, we can't find that page. You'll find lots to explore on the
-            home page.{" "}
+            Sorry, we can not find that page. You will find lots to explore on
+            the home page.
           </p>
-          <link
+          <Link
             href="/"
             className="inline-flex text-white bg-rose-600 hover:bg-rose-800 focus:ring-4 focus:outline-none focus:ring-rose-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center  my-4"
           >
             Back to Homepage
-          </link>
+          </Link>
         </div>
       </div>
     </section>
